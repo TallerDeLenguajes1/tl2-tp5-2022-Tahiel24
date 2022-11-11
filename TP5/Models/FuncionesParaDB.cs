7253b354ce45cb52ea9bf249713f601a81175626
Tahiel24 <tahielvarela@gmail.com>
2022-11-11 04:07:33 -0300

    Primera conexion exitosa a la BD

--- TP5/Models/FuncionesParaDB.cs
+++ TP5/Models/FuncionesParaDB.cs
@@ -1,3 +1,4 @@
+namespace TP5.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,11 +6,28 @@ using System.Threading.Tasks;
 using Microsoft.Data;
 using Microsoft.Data.Sqlite;
 
+
 public class FuncionesDB
 {
-    public FuncionesDB(){
-        //Cadetes
-        //Funcion para recuperar los id de cadeteria de la tabla cadeteria
-        
+    public FuncionesDB(){}
+
+    //Cadetes
+    //Funcion para recuperar los id de cadeteria de la tabla cadeteria
+    static string connectionString= $"Data Source=BD/PedidosDB";
+    static public SqliteConnection conexion= new SqliteConnection(connectionString);
+
+    public List<int> recuperarIDcadeteria()
+    {
+        List<int>listaID=new List<int>();
+        conexion.Open();
+        SqliteCommand select=new SqliteCommand("SELECT Id_cadeteria FROM Cadeteria",conexion);
+        var query=select.ExecuteReader();
+        while(query.Read())
+        {
+                        //ID
+            listaID.Add(query.GetInt32(0));
+        }
+        conexion.Close();
+        return listaID;
     }
 }
