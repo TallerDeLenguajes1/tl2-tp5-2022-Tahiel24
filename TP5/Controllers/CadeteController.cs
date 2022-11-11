5dec2b2d4fd5a9001e78b56126fae53976507fde
Tahiel24 <tahielvarela@gmail.com>
2022-11-11 13:14:14 -0300

    Editado casi implementado

--- TP5/Controllers/CadeteController.cs
+++ TP5/Controllers/CadeteController.cs
@@ -13,74 +13,78 @@ public class CadeteController : Controller
     public CadeteController(ILogger<CadeteController> logger, IMapper mapper)
     {
         _logger = logger;
-        _mapper=mapper;
+        _mapper = mapper;
     }
 
     public IActionResult CadeteRecibir()
-    {  
+    {
         return View();
     }
 
     public IActionResult MostrarCadetesPrincipal()
     {
-        FuncionesDB funciones= new FuncionesDB();
-        List<Cadete>listaCadetes=new List<Cadete>();
-        listaCadetes=funciones.DevolverListadoCadetes();
+        FuncionesDB funciones = new FuncionesDB();
+        List<Cadete> listaCadetes = new List<Cadete>();
+        listaCadetes = funciones.DevolverListadoCadetes();
         return View(listaCadetes);
     }
 
     public IActionResult IntermedioBorrar()
     {
-        Ayuda nuevaAyuda=new Ayuda();
-        List<Cadete>listaCadetes=new List<Cadete>();
-        listaCadetes=nuevaAyuda.DevolverCadetes();
+        Ayuda nuevaAyuda = new Ayuda();
+        List<Cadete> listaCadetes = new List<Cadete>();
+        listaCadetes = nuevaAyuda.DevolverCadetes();
         return View(listaCadetes);
     }
-    
+
     [HttpGet]
     public RedirectToActionResult EliminarCadetes(string Id)
-    {   
-        int idC=Convert.ToInt32(Id);
-        Ayuda nuevaAyuda=new Ayuda();
-        nuevaAyuda.EliminarCadetes(idC);
+    {
+        int idC = Convert.ToInt32(Id);
+        FuncionesDB funciones = new FuncionesDB();
+        funciones.EliminarDatosBD(idC);
         return RedirectToAction("MostrarCadetesPrincipal");
     }
 
     [HttpPost]
     public RedirectToActionResult AgregarCadetes(CadetesViewModels cadeteView)
     {
-        if(ModelState.IsValid){
-            Cadete nuevoCadete= _mapper.Map<Cadete>(cadeteView);
-            FuncionesDB funciones=new FuncionesDB();
+        if (ModelState.IsValid)
+        {
+            Cadete nuevoCadete = _mapper.Map<Cadete>(cadeteView);
+            FuncionesDB funciones = new FuncionesDB();
             funciones.SubirDatosBD(nuevoCadete);
         }
-        
+
         return RedirectToAction("MostrarCadetesPrincipal");
     }
 
     [HttpPost]
-    public RedirectToActionResult EditCad(CadetesViewModels cadeteView)
+    public RedirectToActionResult EditarCadetes(CadetesViewModels cadeteView)
     {
-        if(ModelState.IsValid){
-           Cadete cadeteEditado=_mapper.Map<Cadete>(cadeteView);
-           Ayuda nuevaAyuda=new Ayuda();
-           nuevaAyuda.EscribirEnCSV(cadeteEditado);
-           return RedirectToAction("MostrarCadetesPrincipal");
-        }else{
+        if (ModelState.IsValid)
+        {
+            Cadete cadeteEditado = _mapper.Map<Cadete>(cadeteView);
+            FuncionesDB funciones = new FuncionesDB();
+            funciones.EditarCadetes(cadeteEditado);
+            return RedirectToAction("MostrarCadetesPrincipal");
+        }
+        else
+        {
             return RedirectToAction("Error");
         }
-        
-        
+
+
     }
 
     [HttpGet]
-    [Route("/Cadete/EditarCadetes/{Id}")]    
+    [Route("/Cadete/EditarCadetes/{Id}")]
     public IActionResult EditarCadetes(string Id)
     {
-        int idC=Convert.ToInt32(Id);
-        Ayuda nuevaAyuda=new Ayuda();
-        Cadete nuevoCadete=nuevaAyuda.devolverCadetePorID(Id);
-        CadetesViewModels cadeteView=_mapper.Map<CadetesViewModels>(nuevoCadete);
+        int idC = Convert.ToInt32(Id);
+        FuncionesDB funciones = new FuncionesDB();
+        Cadete nuevoCadete = funciones.DevolverCadetePorId(idC);
+        CadetesViewModels cadeteView = _mapper.Map<CadetesViewModels>(nuevoCadete);
         return View(cadeteView);
     }
 
--- TP5/Models/FuncionesParaDB.cs
+++ TP5/Models/FuncionesParaDB.cs
@@ -13,10 +13,10 @@ public class FuncionesDB
     public FuncionesDB(){}
 
     //Cadetes
-    //Funcion para recuperar los id de cadeteria de la tabla cadeteria
     static string connectionString= $"Data Source=BD/PedidosDB";
     static public SqliteConnection conexion= new SqliteConnection(connectionString);
 
+    //Funcion para devolver un listado de cadetes
     public List<Cadete> DevolverListadoCadetes(){
         List<Cadete>listado=new List<Cadete>();
         conexion.Open();
@@ -32,6 +32,7 @@ public class FuncionesDB
         return listado;
     }
 
+    //Funcion que recupera los id de las cadeterias
     public List<int> recuperarIDcadeteria()
     {
         List<int>listaID=new List<int>();
@@ -47,7 +48,7 @@ public class FuncionesDB
         return listaID;
     }
 
-    //Funcion para agregar datos a la BD
+    //Funcion para agregar cadetes a la BD
     public void SubirDatosBD(Cadete cadete){
         conexion.Open();
         SqliteCommand insertar= new SqliteCommand("INSERT INTO Cadete (Nombre, Direccion, Telefono, id_cadeteria) VALUES (@nom, @dire, @tel, @id_cad)",conexion);
@@ -63,5 +64,51 @@ public class FuncionesDB
             Console.WriteLine("Error: "+ex.Message);
             conexion.Close();
         }
+    }
+
+    //Funcion para eliminar cadetes de la BD
+    public void EliminarDatosBD(int ID){
+        conexion.Open();
+        SqliteCommand delete= new SqliteCommand("DELETE FROM Cadete WHERE Id_cadete=@id", conexion);
+        delete.Parameters.AddWithValue("@id", ID);
+        try
+        {
+            delete.ExecuteReader();
+            conexion.Close();
+        }catch(Exception ex)
+        {
+            Console.WriteLine("Error: "+ex.Message);
+            conexion.Close();
+        }
+    }
+
+    //Funcion para editar cadetes de la BD
+    public void EditarCadetes(Cadete cadete)
+    {
+        conexion.Open();
+        SqliteCommand editar= new SqliteCommand("UPDATE Cadete SET Nombre=@nom, Direccion=@dir, Telefono=@tel, id_cadeteria=@id_cad WHERE Id_Cadete=@cad", conexion);
+        editar.Parameters.AddWithValue("@cad", cadete.Id);
+        editar.Parameters.AddWithValue("@nom", cadete.Nombre);
+        editar.Parameters.AddWithValue("@dir", cadete.Direccion);
+        editar.Parameters.AddWithValue("@tel", cadete.Telefono1);
+        editar.Parameters.AddWithValue("@id_cad", cadete.Id_Cadeteria);
+        try
+        {
+            editar.ExecuteReader();
+            conexion.Close();
+        }catch(Exception ex){
+            Console.WriteLine("Error: "+ex.Message);
+            conexion.Close();
+        }
+    }
+
+    public Cadete DevolverCadetePorId(int ID)
+    {
+        conexion.Open();
+        SqliteCommand recuperar= new SqliteCommand("SELECT * FROM Cadete WHERE Id_cadete=@cad");
+        recuperar.Parameters.AddWithValue("@cad", ID);
+        var query=recuperar.ExecuteReader();
+        Cadete nuevoCadete= new Cadete(query.GetInt32(0), query.GetString(1), query.GetString(2),query.GetString(3),Convert.ToInt32(query.GetString(4)));
+        return nuevoCadete;
     }
 }
